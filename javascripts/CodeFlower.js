a2de04a312e62c724ba23e9b26f76fe4ad922222
shubh610 <40518185+shubh610@users.noreply.github.com>
2018-06-23 16:26:00 +0530

    Update CodeFlower.js

--- javascripts/CodeFlower.js
+++ javascripts/CodeFlower.js
@@ -122,7 +122,7 @@ CodeFlower.prototype.click = function(d) {
 
 CodeFlower.prototype.mouseover = function(d) {
   this.text.attr('transform', 'translate(' + d.x + ',' + (d.y - 5 - (d.children ? 3.5 : Math.sqrt(d.size) / 2)) + ')')
-    .text(d.name + ": " + d.size + " loc")
+    .text(d.name + ": " + d.size)
     .style('display', null);
 };
 
